90dba9114e2109f954c6daedd47b26994a55cc89
ObjethequeBretagne <cornouailleobjethequedev@gmail.com>
2024-07-26 13:45:53 +0200

    changement en build file2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site : "https://objethequebretagne.github.io"
-});
-{
-  build: {
+    site : "https://objethequebretagne.github.io",
+    build: {
     // Exemple : Générer `page.html` au lieu de `page/index.html` pendant la construction.
     format: 'file'
   }
-}
+});
+
+  
+
 
