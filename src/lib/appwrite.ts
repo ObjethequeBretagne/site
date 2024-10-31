ff56dcbc258c8701dad2c5c024eeb745f7c76a98
ObjethequeBretagne <cornouailleobjethequedev@gmail.com>
2024-10-31 01:07:40 +0100

    MAJ novembre

--- src/lib/appwrite.ts
+++
@@ -1,8 +0,0 @@
-import { Client, Databases } from 'appwrite';
-
-export const client = new Client();
-export const databases = new Databases(client);
-
-client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('66cda1fd003cb7d6269e');
