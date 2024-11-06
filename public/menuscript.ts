67fc1875881f26855121aedc53365213a6ade060
ObjethequeBretagne <cornouailleobjethequedev@gmail.com>
2024-11-06 13:28:22 +0100

    checkbox disabled if noscript

---
+++ public/menuscript.ts
@@ -0,0 +1,46 @@
+document.addEventListener('astro:page-load', () => {
+        const checkbox = document.getElementById(
+          "hamburgerMenuToggle"
+        ) as HTMLInputElement;
+        const navWrapper = document.getElementById("navigationWrapper");
+
+        // Fermer automatiquement le menu après avoir cliqué sur un lien
+        document
+          .querySelectorAll("#navigationWrapper ul li")
+          .forEach((item) => {
+            item.addEventListener("click", () => {
+              checkbox.checked = false;
+              navWrapper?.classList.remove("active");
+            });
+          });
+
+        // ferme le menu en cas de rafraichissement de la page
+        window.addEventListener("beforeunload", (event) => {
+          checkbox.checked = false;
+          navWrapper?.classList.remove("active");
+        });
+
+        // Écouteur d'évènements attaché à "change" pour checkbox
+        checkbox.addEventListener("change", () => {
+          if (checkbox.checked) {
+            navWrapper?.classList.add("active");
+          } else {
+            navWrapper?.classList.remove("active");
+          }
+        });
+      });
+
+      const detailsElements = document.querySelectorAll("details");
+
+      detailsElements.forEach((detailsElement) => {
+        detailsElement.addEventListener("click", () => {
+          detailsElements.forEach((otherDetailsElement) => {
+            if (
+              otherDetailsElement !== detailsElement &&
+              otherDetailsElement.open
+            ) {
+              otherDetailsElement.removeAttribute("open");
+            }
+          });
+        });
+      });
