98d3bbeaa997339fd0f4f36b02b1e96d4901a0e7
ObjethequeBretagne <cornouailleobjethequedev@gmail.com>
2024-11-06 14:24:16 +0100

    script menu

--- public/menuscript.js
+++ public/menuscript.js
@@ -1,48 +1,43 @@
-document.addEventListener('astro:page-load', () => {
-  const checkbox = document.getElementById(
-    "hamburgerMenuToggle"
-  );
+document.addEventListener("astro:page-load", () => {
+  const checkbox = document.getElementById("hamburgerMenuToggle");
   const navWrapper = document.getElementById("navigationWrapper");
 
+  // active les intéractions une fois le script chargé
   checkbox.disabled = false;
 
+  // ferme le menu en cas de changement de page
+  checkbox.checked = false;
+  navWrapper?.classList.remove("active");
+
   // Fermer automatiquement le menu après avoir cliqué sur un lien
-  document
-          .querySelectorAll("#navigationWrapper ul li")
-          .forEach((item) => {
-            item.addEventListener("click", () => {
-              checkbox.checked = false;
-              navWrapper?.classList.remove("active");
-            });
-          });
+  document.querySelectorAll("#navigationWrapper ul li").forEach((item) => {
+    item.addEventListener("click", () => {
+      checkbox.checked = false;
+      navWrapper?.classList.remove("active");
+    });
+  });
 
-        // ferme le menu en cas de rafraichissement de la page
-        window.addEventListener("beforeunload", (event) => {
-          checkbox.checked = false;
-          navWrapper?.classList.remove("active");
-        });
+  // Écouteur d'évènements attaché à "change" pour checkbox
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      navWrapper?.classList.add("active");
+    } else {
+      navWrapper?.classList.remove("active");
+    }
+  });
 
-        // Écouteur d'évènements attaché à "change" pour checkbox
-        checkbox.addEventListener("change", () => {
-          if (checkbox.checked) {
-            navWrapper?.classList.add("active");
-          } else {
-            navWrapper?.classList.remove("active");
-          }
-        });
+  const detailsElements = document.querySelectorAll("details");
+
+  detailsElements.forEach((detailsElement) => {
+    detailsElement.addEventListener("click", () => {
+      detailsElements.forEach((otherDetailsElement) => {
+        if (
+          otherDetailsElement !== detailsElement &&
+          otherDetailsElement.open
+        ) {
+          otherDetailsElement.removeAttribute("open");
+        }
       });
-
-      const detailsElements = document.querySelectorAll("details");
-
-      detailsElements.forEach((detailsElement) => {
-        detailsElement.addEventListener("click", () => {
-          detailsElements.forEach((otherDetailsElement) => {
-            if (
-              otherDetailsElement !== detailsElement &&
-              otherDetailsElement.open
-            ) {
-              otherDetailsElement.removeAttribute("open");
-            }
-          });
-        });
-      });
+    });
+  });
+});
